b9f6f517612ad81dbe9b06b3389391735c42ece9
Pratik <pratikg17@github.com>
2017-04-15 10:29:43 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import 'hammerjs';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule,
+      MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
